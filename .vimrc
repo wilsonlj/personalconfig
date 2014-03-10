48949108e9a40ddecb2c4157dcdb8d4eaf26ddb9
wilsonlj <wilson_lj@msn.com>
2014-03-10 22:47:03 +0100

    first commit

---
+++ .vimrc
@@ -0,0 +1,11 @@
+syntax on
+set nocompatible
+set number
+set smartindent
+set expandtab
+set tabstop=4
+set shiftwidth=4
+set incsearch
+set hlsearch
+filetype on
+
---
+++ apt-get-initial.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install vim cmake git build-essential qt4-demos qt4-designer qt4-dev-tools qt4-qmake qt4-qtconfig qtcreator python-matplotlib python-scipy python-numpy python-pygame python-pyopencl typespeed r-base rar unrar texmaker nethogs mplayer okular swi-prolog scala eclipse vpnc network-manager-vpnc
